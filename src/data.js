00ac8f014794850feae9296e38a7b25b030d7260
TheRick88 <avalex2810@gmail.com~>
2022-08-15 21:35:13 +0200

    image fix

---
+++ src/data.js
@@ -0,0 +1,19 @@
+let shopItemsData = [{
+    id: 0,
+    name: "Belgium Flowers",
+    price: 20,
+    imgSrc: "assets/flower1.jpeg",
+},
+{
+    id: 1,
+    name: "Netherlands flowers",
+    price: 25.00,
+    imgSrc: "assets/flower2.jpeg",   
+},
+{
+    id: 2,
+    name: "Italy flowers",
+    price: 30.00,
+    imgSrc: "assets/flower4.jpeg",   
+},]
+
